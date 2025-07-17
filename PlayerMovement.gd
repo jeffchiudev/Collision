1aa7a1f46b857255c187d3e5b9cf1d7c7cfed545
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-16 22:34:36 -0700

    tried adding a victory label to display across screen, reverted.

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -13,4 +13,5 @@ func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Tree"):
 		get_tree().reload_current_scene()
 	if body.is_in_group("FinishLine"):
-		print("You Win")
+		print("You Win!")
+		get_tree().reload_current_scene()
