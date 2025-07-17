6b81252e8fc9a3562854b28c2279c5e1acc913bd
Jeff Chiu <jeffchiudev@gmail.com>
2025-07-16 22:14:58 -0700

    added finish line static body, prints you win in console upon contact

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -12,3 +12,5 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Tree"):
 		get_tree().reload_current_scene()
+	if body.is_in_group("FinishLine"):
+		print("You Win")
